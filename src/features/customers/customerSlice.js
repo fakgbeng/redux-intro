bc451e0392301c04145a27f0188723434e6e4661
Gbenga Fakorede <temris123@gmail.com>
2023-09-17 13:43:17 +0100

    second commit

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,35 @@
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(
+  state = initialStateCustomer,
+  { type, payload }
+) {
+  switch (type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: payload.fullName,
+        nationalID: payload.nationalID,
+        createdAt: payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: payload };
+    default:
+      return state;
+  }
+}
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "account/updateName", payload: fullName };
+}
