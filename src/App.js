4d789e20ef496a9a58b2d185e28a37389abc522c
Gbenga Fakorede <temris123@gmail.com>
2023-09-18 12:14:56 +0100

    third-commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      {!fullName === "" ? (
+      {fullName === "" ? (
         <CreateCustomer />
       ) : (
         <>
